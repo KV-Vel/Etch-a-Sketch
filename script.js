a4a66a2e9c8ed6d35db5c8d408cbe7682d864ea8
SprngVel <kraevvalentin98@gmail.com>
2023-11-29 20:04:11 +1000

    added logic for choosind color, and erasing

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const container = document.querySelector('.container');
 const clearBtn = document.querySelector('.clear-btn');
 const functionsDiv = document.querySelector('.functions');
+const eraserBtn = document.querySelector('.eraser');
+const inputColor = document.querySelector('#color-change');
+let erasePermission = 'OFF';
+let color = 'black';
 let gridSize = 32;
 let containerSize = 700; 
 
@@ -42,13 +46,13 @@ container.addEventListener('mousedown', (e) => {
     let enableDraw = true;
     let targetBlock = e.target;
     if (targetBlock.className == 'child-container' && enableDraw === true) {
-        targetBlock.style.cssText += 'background: black;';
+        targetBlock.style.cssText += `background: ${color};`;
     }
 
     container.addEventListener('mouseover', (e) => {
     let targetBlock = e.target;                                  
     if (targetBlock.className == 'child-container' && enableDraw === true) {
-        targetBlock.style.cssText += 'background: black;';
+        targetBlock.style.cssText += `background: ${color};`;
     }
     })
 
@@ -58,11 +62,32 @@ container.addEventListener('mousedown', (e) => {
 });
 //TO DO: when mouse leaves container stop drawing
 
+eraserBtn.addEventListener('click', erase);
+
+function erase (e) {
+    if (e.target.classList.toggle('active')) {
+        e.target.textContent = 'Eraser is ON';
+        color = `#ffffff`;
+    } else {
+        e.target.textContent = 'Eraser is OFF';
+        e.target.classList.toggle('eraser');
+        color = `#000`;
+    }
+}
+
 clearBtn.addEventListener('click', clear)
 
 function clear () {
     const divBlock = document.querySelectorAll('.child-container');
     divBlock.forEach ((element) => {
-        element.style.cssText += 'background: white'
+        element.style.cssText += 'background: white';
     })
 }
+
+inputColor.addEventListener('input', getColor);
+
+function getColor (e) {color = e.target.value;}
+
+// HTML - added active style for toggling eraser and changed button styling to class .btn to make it more specific than .active class
+// CSS - added styling for active eraser
+// JS - added logic for input color to choose which color to draw, added logic for eraser button.
